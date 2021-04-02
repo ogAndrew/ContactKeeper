c92f2e9916caebe28ae38717d1d68a56603bae0f
Andrew Talle <talle.andrew@gmail.com>
2021-04-02 13:26:45 -0600

    add login and register page

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -40,6 +40,7 @@ const ContactState = (props) => {
       },
     ],
     current: null,
+    filtered: null,
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -71,19 +72,28 @@ const ContactState = (props) => {
   };
 
   // Filter Contacts
+  const filteredContacts = (text) => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
 
   // Clear Filter
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
 
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
-        updateContact
+        updateContact,
+        filteredContacts,
+        clearFilter,
       }}
     >
       {props.children}
