a3990fe07aba1aac641f00195a84269fcb800d50
Andrew Talle <talle.andrew@gmail.com>
2021-04-04 21:13:02 -0600

    register user through ui

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,7 +2,7 @@ import React, { useReducer } from 'react';
 import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
-
+import setAuthToken from '../../utils/setAuthToken';
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
@@ -27,7 +27,7 @@ const AuthState = (props) => {
 
   // Load User
   const loadUser = async () => {
-    
+    setAuthToken(localStorage.token);
 
     try {
       const res = await axios.get('/api/auth');
